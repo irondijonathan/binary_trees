a1500ca877563c246abe7e19723756d3e704dba2
Ato Toffah <atotoffah@gmail.com>
2023-10-31 23:04:25 +0000

    Fix 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,15 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new = NULL;
 
-	if (!new || !parent)
+	if (!parent)
 		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
 		parent->left->parent = new;
