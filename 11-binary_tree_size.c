723e8d870d12ada1b35f55be426cf0035921d701
Ato Toffah <atotoffah@gmail.com>
2023-11-01 11:11:25 +0000

    Task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * count_nodes - counts the nodes of a binary tree
+ * @tree: pointer to the current node
+ * @count: pointer to the total count
+ *
+ * Return: void
+ */
+void count_nodes(const binary_tree_t *tree, size_t *count)
+{
+	if (!tree)
+		return;
+	*count = *count + 1;
+	count_nodes(tree->left, count);
+	count_nodes(tree->right, count);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: the root node of the tree to measure
+ *
+ * Return: size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+	count_nodes(tree, &count);
+	return (count);
+}
