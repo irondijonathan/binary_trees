59e6f919c669fe1173d51f762c6c834ed2cf35fe
Jessica Asamoah <jessicaasamoah903@gmail.com>
2023-11-02 09:53:00 +0000

    Task 18

--- binary_trees.h
+++ binary_trees.h
@@ -61,4 +61,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 
 int binary_tree_is_full(const binary_tree_t *tree);
 
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 #endif /* BINARY_TREES_H */
